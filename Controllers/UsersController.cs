9eef482e5f70a8a911803365f9d0771bbef68523
Sameh Frehat <samehfrehat@ymail.com>
2019-04-19 12:39:49 +0300

    CRUD Oprations Api working

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -9,20 +9,72 @@ namespace UsersManagement.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        //create the  user service 
         private readonly UserService _userService;
 
+        //initialize the _userService within constructor  
         public UsersController(UserService userService)
         {
             _userService = userService;
         }
 
-        [HttpGet]
+        [HttpGet] //returns all the users /api/users
         public ActionResult<List<User>> Get()
         {
             return _userService.Get();
         }
 
+        [HttpGet("{id}", Name = "GetUser")]//returns users by Id /api/users/{id}
+        public ActionResult<User> Get(string id)
+        {
+            var user = _userService.Get(id);
 
-        
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpPost]//creates user and return it by Id /api/users
+        public ActionResult<User> Create(User user)
+        {
+            _userService.Create(user);
+            return CreatedAtRoute("GetUser", new { id = user.Id.ToString() }, user);
+        }
+
+        [HttpPut("{id}")]//Update the user by replacing it by another /api/users/id
+        public IActionResult Update(string id, User userIn)
+        {
+            var user = _userService.Get(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            //assign the old if to the new object to keep it at the same position
+            userIn._id = user._id;
+            _userService.Update(id, userIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]//delete the user by Id /api/users/id
+        public IActionResult Delete(string id)
+        {
+            var user = _userService.Get(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _userService.Remove(user.Id);
+
+            return NoContent();
+        }
+
     }
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Linq;
 using UsersManagement.Models;
 using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
